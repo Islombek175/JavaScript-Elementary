877d0d46509acdd562e6ed2aeaa2beac5c33e5df
Islombek Matkarimov <matkaimovislambek@gmail.com>
2025-12-26 16:43:51 +0500

    3-Dars, o'zgaruvchilar (var, let, const)

---
+++ №3 O'zgaruvchilar/script.js
@@ -0,0 +1,52 @@
+// O'zgaruvchilar
+
+// 1 - let
+// 2 - const
+
+let age = 17
+const name = 'Islombek'
+
+console.log(age)
+
+age = 18
+
+console.log(age)
+
+console.log('---------------------------')
+
+console.log(name)
+
+// name = "Ismoil"
+// xato
+
+console.log(name)
+
+console.log('---------------------------')
+
+const person = {
+	name: 'Islombek',
+	age: 17,
+}
+
+console.log(person)
+
+person.name = 'Ismoil'
+
+console.log(person)
+
+console.log('---------------------------')
+
+console.log(fullName)
+
+var fullName = 'Islobek Matkarimov'
+
+console.log('---------------------------')
+
+{
+	// const surname = 'Matkarimov'
+	// demak, var bu global o'zgaruvchi va uni barcha yerda qo'llasa bo'ladi
+	var surname = 'Matkarimov'
+	console.log(surname)
+}
+
+console.log(surname)
