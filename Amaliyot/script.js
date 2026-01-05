2285a484434b63dfe69ea064e704493d6ba00365
Islombek Matkarimov <matkaimovislambek@gmail.com>
2026-01-05 16:05:26 +0500

    №16. Amaliyot. OOP

--- Amaliyot/script.js
+++ Amaliyot/script.js
@@ -1,70 +1,72 @@
 let numberOfSeries
 
-startApp()
-
 const seriesDB = {
 	count: numberOfSeries,
 	series: {},
 	actors: {},
 	genres: [],
-	privat: true,
-}
-
-detectingLevel()
-setFavouriteSeries()
-writeGenres()
-showDb()
-
-function startApp() {
-	while (
-		numberOfSeries == null ||
-		numberOfSeries == '' ||
-		isNaN(numberOfSeries) ||
-		numberOfSeries == undefined
-	) {
-		numberOfSeries = +prompt('Nechta serial ko’rdingiz?', '')
-	}
-}
-
-function setFavouriteSeries() {
-	for (let i = 0; i < 2; i++) {
-		let serie = prompt(`Oxirgi ko’rgan serialingiz? ${1 + i}`, '')
-		let grade = +prompt(`Nechi baxo berasiz? ${1 + i}`, '')
-
-		if (serie != '' && serie != null && grade != '' && grade != null) {
-			seriesDB.series[serie] = grade
-		} else {
-			i--
+	private: true,
+	start: function () {
+		while (
+			numberOfSeries == null ||
+			numberOfSeries == '' ||
+			isNaN(numberOfSeries) ||
+			numberOfSeries == undefined
+		) {
+			numberOfSeries = +prompt('Nechta serial ko’rdingiz?', '')
 		}
-	}
-}
-
-function detectingLevel() {
-	if (numberOfSeries > 10) {
-		console.log('Siz serialchi zvezda ekansiz')
-	} else if (numberOfSeries >= 5) {
-		console.log('Siz classik tamoshabin ekansiz')
-	} else if (numberOfSeries < 5) {
-		console.log('Kam serial ko’ripsiz')
-	}
-}
-
-function showDb() {
-	if (seriesDB.privat) {
-		console.log(seriesDB)
-	} else {
-		console.log('Private!')
-	}
-}
-
-function writeGenres() {
-	for (let i = 0; i < 3; i++) {
-		let genre = prompt(`Yaxshi ko’rgan janringiz? ${1 + i}`, '')
-
-		if (genre != null && genre != '') {
-			seriesDB.genres[i] = genre
-		} else {
-			i--
+	},
+	detectingLevel: function () {
+		if (numberOfSeries > 10) {
+			console.log('Siz serialchi zvezda ekansiz')
+		} else if (numberOfSeries >= 5) {
+			console.log('Siz classik tamoshabin ekansiz')
+		} else if (numberOfSeries < 5) {
+			console.log('Kam serial ko’ripsiz')
 		}
-	}
+	},
+	setFavouriteSeries: function () {
+		for (let i = 0; i < 2; i++) {
+			let serie = prompt(`Oxirgi ko’rgan serialingiz? ${1 + i}`, '')
+			let grade = +prompt(`Nechi baxo berasiz? ${1 + i}`, '')
+
+			if (serie != '' && serie != null && grade != '' && grade != null) {
+				seriesDB.series[serie] = grade
+			} else {
+				i--
+			}
+		}
+	},
+	writeGenres: function () {
+		for (let i = 0; i < 3; i++) {
+			let genre = prompt(`Yaxshi ko’rgan janringiz? ${1 + i}`, '')
+
+			if (genre != null && genre != '') {
+				seriesDB.genres[i] = genre
+			} else {
+				i--
+			}
+		}
+		seriesDB.genres.forEach((genre, index, arr) => {
+			console.log(`Yaxshi ko’rgan janringiz #${1 + index} - ${genre}`)
+		})
+	},
+	visibleDB: function () {
+		if (seriesDB.private) {
+			seriesDB.private = false
+		} else {
+			seriesDB.private = true
+		}
+	},
+	showDb: function () {
+		if (seriesDB.private) {
+			console.log(seriesDB)
+		} else {
+			console.log('Private!')
+		}
+	},
 }
+
+const movie = Object.create(seriesDB)
+
+console.log(movie)
